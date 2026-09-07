f8b71c201488f9cbc8fddc4bc15d433bf2718484
k74339562-cloud <k74339562@gmail.com>
2026-09-07 11:42:05 +0300

    Refactor ChunkSection.hpp by removing unused includes
    Removed OpenGL includes and commented lines in ChunkSection.

--- include/ChunkSection.hpp
+++ include/ChunkSection.hpp
@@ -2,8 +2,8 @@
 
 #include "Block.hpp"
 #include "Vertex.hpp"
+#include "OpenGL.hpp"
 #include <vector>
-#include <glad/glad.h>
 
 constexpr int SECTION_SIZE = 16;
 constexpr int SECTION_VOLUME = SECTION_SIZE * SECTION_SIZE * SECTION_SIZE;
@@ -13,14 +13,10 @@ public:
     ChunkSection(int chunkX, int chunkY, int chunkZ);
     ~ChunkSection();
 
-    // ضبط وقراءة نوع البلوكة في الإحداثيات المحلية (0 إلى 15)
     void setBlock(int x, int y, int z, BlockType type);
     BlockType getBlock(int x, int y, int z) const;
 
-    // بناء المش مع إخفاء كل الأوجه غير المرئية (Face Culling)
     void buildMesh();
-
-    // رسم المش في كرت الشاشة
     void render() const;
 
     bool isEmpty() const { return m_nonAirCount == 0; }
@@ -30,15 +26,11 @@ private:
     BlockType m_blocks[SECTION_VOLUME];
     int m_nonAirCount = 0;
 
-    // أدوات OpenGL للمش المضغوط
     GLuint m_vao = 0;
     GLuint m_vbo = 0;
     GLsizei m_vertexCount = 0;
 
-    // فحص هل البلوكة المجاورة صلبة لحجب الوجه
     bool isFaceVisible(int x, int y, int z, Direction dir) const;
-
-    // إضافة وجه مكعب مضغوط بالكامل
     void addFace(std::vector<PackedVertex>& vertices, 
                  int x, int y, int z, Direction dir, int texLayer);
 };
