6bcf7d2ad19debd343494f416931f95eaed918e4
k74339562-cloud <k74339562@gmail.com>
2026-09-07 15:20:30 +0300

    Refactor Block enum and remove unused properties

--- include/Block.hpp
+++ include/Block.hpp
@@ -10,44 +10,17 @@ enum class BlockType : uint8_t {
     Grass,
     Dirt,
     Stone,
-    Glass,       // زجاج (نصف شفاف)
-    OakLeaves,   // ورق شجر (مفرغ)
+    OakLeaves,
+    OakLog,      // خشب الشجرة الحقيقي
+    Glass,
     Count
 };
 
-enum class BlockRenderType : uint8_t {
-    Air,
-    Opaque,      // صلب تماماً
-    Cutout,      // ورق شجر ونباتات
-    Translucent  // زجاج وماء
-};
-
-struct BlockProperties {
-    BlockRenderType renderType;
-    int topTexture;
-    int bottomTexture;
-    int sideTexture;
-};
-
-// مصفوفة ثابتة فائقة السرعة للخصائص
-inline constexpr BlockProperties BLOCK_DATA[] = {
-    // Air
-    { BlockRenderType::Air, 0, 0, 0 },
-    // Grass
-    { BlockRenderType::Opaque, 0, 2, 1 },
-    // Dirt
-    { BlockRenderType::Opaque, 2, 2, 2 },
-    // Stone
-    { BlockRenderType::Opaque, 3, 3, 3 },
-    // Glass
-    { BlockRenderType::Translucent, 4, 4, 4 },
-    // OakLeaves
-    { BlockRenderType::Cutout, 5, 5, 5 }
-};
-
-// دوال مساعدة سريعة جداً (Inline)
 inline bool isBlockOpaque(BlockType type) {
-    return BLOCK_DATA[static_cast<size_t>(type)].renderType == BlockRenderType::Opaque;
+    return type == BlockType::Grass || 
+           type == BlockType::Dirt  || 
+           type == BlockType::Stone || 
+           type == BlockType::OakLog; // الخشب صلب ومعتم
 }
 
 inline bool isBlockAir(BlockType type) {
